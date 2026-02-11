44fdaca25bfae8d2faa5cff5e97c8125d485d09c
Helmy-Fathy <7elmyfathy@gmail.com>
2026-02-11 06:07:43 +0200

    ENUM Question01

--- G-NET-26-C#-5/Program.cs
+++ G-NET-26-C#-5/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            
+            #region ENUMS
+            #region Q1
+            /*
+             Q1 : Day of the Week
+                Create an enum called DayOfWeek with values: Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday.
+                Then write a program that:
+                •	Asks the user to enter a day number (0–6).
+                •	Converts it to the enum and prints the day name.
+                •	Uses a switch statement to print whether it's a "Workday" or a "Weekend".
+            */
+
+            //Console.Write("Enter a day number (1-7): ");
+            //int dayNumber = int.Parse(Console.ReadLine());
+
+            //DayOfWeek day = (DayOfWeek)(dayNumber - 1);
+            //Console.WriteLine($"Day: {day}");
+
+            //switch (day)
+            //{
+            //    case DayOfWeek.Friday:
+            //    case DayOfWeek.Saturday:
+            //        Console.WriteLine("It's the Weekend");
+            //        break;
+            //    case DayOfWeek.Sunday:
+            //    case DayOfWeek.Monday:
+            //    case DayOfWeek.Tuesday:
+            //    case DayOfWeek.Wednesday:
+            //    case DayOfWeek.Thursday:
+            //        Console.WriteLine("It's a Workday");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid day number");
+            //        break;
+            //}
+            #endregion
+            #endregion
         }
     }
 }
